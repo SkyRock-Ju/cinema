9d5963c7824172f9075c1e73cf193d7c6f020b3b
Dulustan <skyrockpk@gmail.com>
2023-04-22 12:21:03 +0700

    decomposing script creating tables

---
+++ src/main/resources/db/scripts/006_ddl_create_table_users.sql
@@ -0,0 +1,8 @@
+create table users
+(
+    id        serial primary key,
+    full_name varchar        not null,
+    email     varchar unique not null,
+    password  varchar        not null,
+    is_admin  boolean        not null
+);
